abc92d40d8e0a90031e1732ab77503553ba6c724
DaichiSawamura <dimakr-303@yandex.ru>
2023-01-19 17:13:45 +0300

    creat main.py 'Hello World'

--- src/main.py
+++ src/main.py
@@ -1,11 +1,1 @@
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello Linux"
-        )
-=======
-print("Hello Linux")
->>>>>>> parent of 7748c78 (hello world)
-=======
-print("Hello world")
->>>>>>> parent of f61881d (changed to hello linux)
+print("Hello World")
