8a9116d9de3f22f79a92b09848684dbca9ff020b
DaichiSawamura <dimakr-303@yandex.ru>
2023-01-19 17:05:52 +0300

    revert

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("Hello Linux"
         )
 =======
 print("Hello Linux")
 >>>>>>> parent of 7748c78 (hello world)
+=======
+print("Hello world")
+>>>>>>> parent of f61881d (changed to hello linux)
